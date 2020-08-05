4b459892a666a70955e9eeec60d4ef24c08a7069
Clean <67143152+jdolendo1996@users.noreply.github.com>
2020-08-06 01:14:44 +0700

    ADDED ANIMATION.CSS

--- js/landing_page.js
+++ js/landing_page.js
@@ -17,17 +17,83 @@ $( window ).scroll(function() {
 	} else {
 		$('#navbar_lp').removeClass('sticky-navbar');
 	}
+
+	 // If element is scrolled into view, do actions
+	$('.animate__animated').each(function() {
+      if (isScrolledIntoView(this) === true) {
+      	
+      	//apply animation
+      	if($(this).hasClass('effect-fil')){
+      		$(this).addClass('animate__fadeInLeft');
+      	}else if($(this).hasClass('effect-fir')){
+      		$(this).addClass('animate__fadeInRight');
+      	}else if($(this).hasClass('effect-fid')){
+      		$(this).addClass('animate__fadeInDown');
+      	}
+      }
+    });
 });
 
-// When nav item is clicked, change active state
+
+//NAVBAR ACTIONS
 $('#top_navbar > ul > li').on('click',function(){
-console.log(this);
+
 	if($(this).is('[class*=link]')){
-		 $('.nav-item').removeClass('active');  //Remove any previous active classes
-  
- 		 $(this).addClass('active'); //Add active class to the clicked item
+		 $('.nav-item').removeClass('active');
+ 		 $(this).addClass('active'); 
 	}
   
 });
-/*END NAV BAR ACTIONS*/
+
+//SKILLS NAVBAR ACTIONS
+$('#nav_skills > ul > li').on('click',function(){
+
+	//remove active state
+	$('.nav-item').removeClass('active');
+ 	$(this).addClass('active');
+
+ 	var class_val = '';
+
+	if($(this).hasClass('s-tech')){
+		class_val = 's-tech';
+	}else if($(this).hasClass('s-personal')){
+		class_val = 's-personal';
+	}else if($(this).hasClass('s-others')){
+		class_val = 's-others';
+	}
+
+
+	$('.skill-type').each(function() {
+     
+		if($(this).hasClass(class_val)){
+
+			$(this).removeClass('d-none');
+
+			//apply animation
+	      	if($(this).hasClass('effect-fil')){
+	      		$(this).addClass('animate__fadeInLeft');
+	      	}else if($(this).hasClass('effect-fir')){
+	      		$(this).addClass('animate__fadeInRight');
+	      	}
+		}else{
+			$(this).addClass('d-none');
+		}
+
+      
+    });
+  
+});
+
+
+ // Check if element is scrolled into view
+ function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
 });
